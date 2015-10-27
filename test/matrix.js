a27af3ed30c0aafef9f52fa66914d755557a529f
samisdat <bastian.sackermann@gmail.com>
2015-10-27 20:42:20 +0000

    test for matrix

--- test/matrix.js
+++ test/matrix.js
@@ -2,109 +2,200 @@ var expect = require('chai').expect;
 var assert = require('chai').assert;
 
 var Matrix = require('../lib/matrix');
-var Color = require('../lib/color');
 
-describe('Matrix', function() { it('can be created', function() {
+var LiquidColor = require('../lib/liquid-color');
+var Seam = require('../lib/seam');
 
-    var matrix = new Matrix();
+describe('Matrix', function() {
 
-    expect(matrix).to.be.instanceof(Matrix);
+    var colors = [];
 
-});
+    beforeEach(function() {
+        colors = [
+            [
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 255, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+            ],
+            [
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 255, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+            ],
+            [
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 255, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+            ],
+            [
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 255, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+                new LiquidColor(0, 0, 0, 255),
+            ],
+        ];
+
+    });
+
+    it('can be created', function() {
+
+        var matrix = new Matrix(colors);
+
+        expect(matrix).to.be.instanceof(Matrix);
+
+    });
 
     it('can get width/height', function() {
 
-        var width = 4;
-        var height = 3;
+        var matrix = new Matrix(colors);
 
-        var matrix = new Matrix(width, height);
-
-        expect(matrix.getWidth()).to.be.equal(width);
-        expect(matrix.getHeight()).to.be.equal(height);
+        expect(matrix.getWidth()).to.be.equal(5);
+        expect(matrix.getHeight()).to.be.equal(4);
 
     });
 
     it('can get/set color', function() {
 
-        var matrix = new Matrix(4, 3);
+        var matrix = new Matrix(colors);
 
-        var rgba = matrix.getColor(1, 1);
-        expect(rgba.r).to.be.undefined;
-        expect(rgba.g).to.be.undefined;
-        expect(rgba.b).to.be.undefined;
-        expect(rgba.a).to.be.undefined;
+        var color = matrix.getColor(0, 0);
 
-        var r = 10;
-        var g = 11;
-        var b = 12;
-        var a = 13;
+        expect(color.r).to.equal(0);
+        expect(color.g).to.equal(0);
+        expect(color.b).to.equal(0);
+        expect(color.a).to.equal(255);
 
-        matrix.setColor(1, 1, r, g, b, a);
-        var rgba = matrix.getColor(1, 1);
-        expect(rgba.r).to.equal(r);
-        expect(rgba.g).to.equal(g);
-        expect(rgba.b).to.equal(b);
-        expect(rgba.a).to.equal(a); }); it('can get/set heat', function() {
+    });
 
-            var matrix = new Matrix(4, 3);
+    it('can getHeat', function() {
 
-            expect(matrix.getHeat(1, 1)).to.be.undefined;
+        var matrix = new Matrix(colors);
+        expect(matrix.getHeat(1,1)).to.be.equal(1020);
+        expect(matrix.getHeat(2,2)).to.be.equal(0);
 
-            var heat = 10;
-            matrix.setHeat(1, 1, heat);
-            expect(matrix.getHeat(1,1)).to.be.equal(heat); }); it('can check deteled and mark as deleted', function() {
+    });
 
-                var matrix = new Matrix(4, 3);
+    it('can check isDeleted and markAsDeleted', function() {
 
-                expect(matrix.isDeleted(1, 1)).to.be.false;
+        var matrix = new Matrix(colors);
 
-                matrix.markAsDeleted(1,1);
-                expect(matrix.isDeleted(1,1)).to.be.true;
+        expect(matrix.isDeleted(1,1)).to.be.false;
 
-            });
+        matrix.markAsDeleted(1, 1);
+        expect(matrix.isDeleted(1,1)).to.be.true;
 
-    it('can sumColorChanels', function() {
-
-        var matrix = new Matrix(1, 1);
-
-        var r = 1;
-        var g = 2;
-        var b = 3;
-        var a = 4;
-
-        matrix.setColor(0, 0, r, g, b, a);
-
-        expect(matrix.sumColorChanels(0, 0)).to.be.equal(10);
-
-        expect(matrix.sumColorChanels(1, 0)).to.be.equal(0);
-        expect(matrix.sumColorChanels(0, 1)).to.be.equal(0);
-
-        expect(matrix.sumColorChanels(-1, 0)).to.be.equal(0);
-        expect(matrix.sumColorChanels(0, -1)).to.be.equal(0);
 
     });
 
     it('can getMaxHeat', function() {
 
-        var matrix = new Matrix(5, 5);
+        var matrix = new Matrix(colors);
 
-        for (var x = 0, width = matrix.getWidth(); x < width; x += 1) {
-
-            for (var y = 0; y < matrix.getHeight(); y++) {
-
-                matrix.setColor(y, x, 0, 0, 0, 0);
-
-            }
-        }
-        matrix.setColor(2, 2, 255, 255, 255, 255);
-
-        matrix.generateHeatMap();
-
-        /* very dummy test, but covers 100% of heatmap ;) */
-        expect(matrix.getMaxHeat()).to.be.equal(2040);
+        // very clever test, but covers 100% of heatmap ;)
+        expect(matrix.getMaxHeat()).to.be.equal(1020);
 
     });
 
+    it('can generateSeams and getSeams', function() {
+
+        var matrix = new Matrix(colors);
+
+        matrix.generateSeams();
+
+        expect(matrix.numberOfSeams()).to.be.equal(2);
+
+        var seams = matrix.getSeams();
+
+        expect(seams[0]).to.be.instanceof(Seam);
+        expect(seams[1]).to.be.instanceof(Seam);
+
+        matrix.generateSeams();
+        expect(matrix.numberOfSeams()).to.be.equal(2);
+
+    });
+
+    it('can flip matrix', function() {
+
+        var matrix = new Matrix(colors);
+
+        var flipped = matrix.flip();
+
+        expect(flipped).to.be.instanceof(Matrix);
+
+    });
+
+    it('can getReduceWidth', function() {
+
+        var matrix = new Matrix(colors);
+
+        matrix.generateSeams();
+        var reduced = matrix.getReduceWidth(4);
+
+        expect(reduced).to.be.instanceof(Matrix);
+        expect(reduced.getWidth()).to.be.equal(4);
+        expect(reduced.getHeight()).to.be.equal(4);
+
+    });
+
+    it('can getReduceWidth, request more then seams', function() {
+
+        var matrix = new Matrix(colors);
+
+        matrix.generateSeams();
+        var reduced = matrix.getReduceWidth(2);
+
+        expect(reduced).to.be.instanceof(Matrix);
+        expect(reduced.getWidth()).to.be.equal(3);
+        expect(reduced.getHeight()).to.be.equal(4);
+
+    });
+
+    it('can getReduceHeight', function() {
+
+        var matrix = new Matrix(colors);
+
+        matrix.generateSeams();
+        var reduced = matrix.getReduceHeight(3);
+
+        expect(reduced).to.be.instanceof(Matrix);
+        expect(reduced.getWidth()).to.be.equal(5);
+        expect(reduced.getHeight()).to.be.equal(3);
+
+    });
+
+    it('can getReduceHeight, request more then seams', function() {
+
+        var matrix = new Matrix(colors);
+
+        var reduced = matrix.getReduceHeight(2);
+
+        expect(reduced).to.be.instanceof(Matrix);
+        expect(reduced.getWidth()).to.be.equal(5);
+        expect(reduced.getHeight()).to.be.equal(2);
+
+    });
+
+    it('can getReduced', function() {
+
+        var matrix = new Matrix(colors);
+
+        reduced = matrix.getReduced({
+            width:4,
+            height:3
+        });
+
+        expect(reduced).to.be.instanceof(Matrix);
+        expect(reduced.getWidth()).to.be.equal(4);
+        expect(reduced.getHeight()).to.be.equal(3);
+
+    });
 
 });
 
